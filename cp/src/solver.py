9dbf713e2caa08c5158adc204f57aba73942445b
kilian36 <tizianokil@gmail.com>
2023-04-15 16:48:08 +0200

    Second model fixed and interface update

--- cp/src/solver.py
+++ cp/src/solver.py
@@ -1,50 +1,85 @@
 import datetime
 
 from minizinc import Model, Solver, Status, Instance
+from utils import *
+#load_preprocessing,print_graph
 
 
 class CPsolver:
 
-    def __init__(self, data, output_dir="./cp/out/", timeout=300):
-        self.data = data
+    def __init__(self, data, output_dir="./cp/out/", timeout=300, model = 0):
+        
         self.output_dir = output_dir
         self.timeout = timeout
-        self.solver_path = "./cp/src/model.mzn"
-
+        if model == 0:
+            self.data = data
+            self.solver_path = "./cp/src/models/model.mzn"
+        else:
+            self.data = load_preprocessing(data)
+            self.solver_path = "./cp/src/models/graph_model.mzn"
+    
+    
     def solve(self):
         model = Model(self.solver_path)
         solver = Solver.lookup("chuffed")
+        
+        if self.solver_path == "./cp/src/models/model.mzn":
+            result = self.model3D_solve(model, solver)
+        else:
+            result = self.graph_model_solve(model, solver)
 
-        solutions = []
+        return result
+
+
+    def model3D_solve(self, model, solver):
         i = 0
+        solutions = []
         for d in self.data:
             if i % 5 == 0:
                 print("Solving instances ", i)
             try:
                 instance = Instance(solver, model)
-                if self.solver_path == "./cp/src/model.mzn":
-                    result = self.model3D_solve(self, d, instance)
-                else:
-                    result = self.graph_model_solve(self, d, instance)
-
+                result = self.model3D_solve_instance(d, instance)
                 if result.status is Status.OPTIMAL_SOLUTION:
-                    # assignments = result["assignments"]
+                    assignments = result["assignments"]
                     obj_dist = result["obj_dist"]
-
-                    """
-                    We need a format to write the solution and after that save the results in a list
-                    This is a partial solution to observe the result 
-                    """
+                    print("Assignments = \n",assignments)
+                    print("Obj distance = ",obj_dist)
+                    print(result.statistics['time'].total_seconds())
                     solutions.append(obj_dist)
-
-            except Exception:
+            except:
                 pass
-
-            i = i + 1
-
+            
+            i += 1
         return solutions
 
-    def model3D_solve(self, d, instance):
+
+    def graph_model_solve(self, model, solver):
+        i = 0
+        solutions = []
+        for d in self.data:
+            if i % 5 == 0:
+                print("Solving instances ", i)
+            try:
+                instance = Instance(solver, model)
+                result = self.graph_model_solve_instance(d, instance)
+
+                if result.status is Status.OPTIMAL_SOLUTION:
+                    ns = result["ns"]
+                    es = result["es"]
+                    path_dist = result["path_dist"]
+                    print_graph(ns,es,instance["starting_nd"],instance["ending_nd"],path_dist)
+                    print(result.statistics['time'].total_seconds())
+                    solutions.append(ns)
+
+            except:
+                pass
+            
+            i += 1
+        return solutions
+    
+    
+    def model3D_solve_instance(self, d, instance):
         courier, item, courier_size, item_size, distances = d
         instance["courier"] = courier
         instance["items"] = item
@@ -53,8 +88,9 @@ class CPsolver:
         instance["distances"] = distances
         return instance.solve(timeout=datetime.timedelta(seconds=self.timeout), processes=10, random_seed=42,
                               free_search=True)
+    
 
-    def graph_model_solve(self, d, instance):
+    def graph_model_solve_instance(self, d, instance):
         n_couriers, n_items, couriers_size, objects_size, starting_nd, ending_nd, weigths = d
         instance["courier"] = n_couriers
         instance["items"] = n_items
@@ -65,3 +101,5 @@ class CPsolver:
         instance["weigths"] = weigths
         return instance.solve(timeout=datetime.timedelta(seconds=self.timeout), processes=10, random_seed=42,
                               free_search=True)
+    
+    
